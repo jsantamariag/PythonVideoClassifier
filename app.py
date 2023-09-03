41bc3a76bb0fb29fe88cff4970abe3c819ba4365
jsantamariag <j.santamariag@alumnos.upm.es>
2023-09-03 18:58:32 +0200

    impemented web socket to send message throughout every phase

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from flask_socketio import SocketIO, emit
 from pytube import YouTube
 import moviepy.editor as mp
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
@@ -7,17 +8,19 @@ import torch
 import os
 import whisper
 
-# Initialize Flask and CORS
+# Initialize Flask, CORS and SocketIO
 app = Flask(__name__)
 CORS(app)
+socketio = SocketIO(app, cors_allowed_origins="*")
+
 
 # Load models and tokenizers
 tokenizer_text = AutoTokenizer.from_pretrained("jsantamariag/RobertaAnubisLast")
 model_text = AutoModelForSequenceClassification.from_pretrained("jsantamariag/RobertaAnubisLast")
 model_text.eval()
-
 asr_model = model = whisper.load_model("large")
 
+# Start program
 @app.route('/analyze_video', methods=['POST'])
 def analyze_video():
     data = request.json
@@ -28,12 +31,14 @@ def analyze_video():
 
     # Download the full video
     print("Downloading full video...")
+    socketio.emit('message', {'data': 'Downloading full video...'})
     yt = YouTube(youtube_link)
     video = yt.streams.get_highest_resolution()
     out_file = video.download(output_path='.')
 
     # Convert video to audio (.wav)
     print("Extracting audio...")
+    socketio.emit('message', {'data': 'Extracting audio...'})
     audio_path = "processed_audio.wav" #os.path.splitext(out_file)[0] + ".wav"
 
     if os.path.exists(audio_path):
@@ -44,6 +49,7 @@ def analyze_video():
 
     # Perform ASR
     print("Performing ASR...")
+    socketio.emit('message', {'data': 'Performing ASR...'})
     asr_result = model.transcribe("processed_audio.wav")
     print(asr_result["text"])
 
@@ -51,6 +57,8 @@ def analyze_video():
 
     # Classify the text
     print("Classifying text...")
+    socketio.emit('message', {'data': 'Classifying text...'})
+
     inputs_text = tokenizer_text(transcribed_text, return_tensors="pt", truncation=True, padding=True, max_length=512)
     with torch.no_grad():
         outputs_text = model_text(**inputs_text)
@@ -61,6 +69,7 @@ def analyze_video():
     confidence = probabilities[0][predicted_class].item() * 100
 
     print("Preparing report...")
+    socketio.emit('message', {'data': 'Preparing report...'})
 
     result = {
         'transcription': transcribed_text,
@@ -71,4 +80,4 @@ def analyze_video():
     return jsonify(result)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, debug=True)
